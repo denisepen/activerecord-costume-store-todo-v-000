22abc90953b5d7296b426dd974564ee68295841a
Denise Pendleton <dpendl1@hotmail.com>
2018-04-05 15:56:44 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,10 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
-      
+      t.boolean :still_in_business
+      t.text :opening_time
+      t.text :closing_time
+
     end
 
   end
