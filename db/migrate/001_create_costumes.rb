a8a45ff834dcb4fe08f09d4c1610eded56c3b0ee
Denise Pendleton <dpendl1@hotmail.com>
2018-04-05 15:52:48 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,7 +13,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
     create_table :costumes do |t|
       t.string :name
       t.integer :price
-      t.string :image_url
+      t.text :image_url
       t.string :size
       t.timestamps
     end
